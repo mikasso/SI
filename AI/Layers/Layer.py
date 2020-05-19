c0fdd95e3c279e37b4190f758ea057630d5af238
mikasso <mikokaj.szaroleta@gmail.com>
2020-05-19 11:20:18 +0200

    init commit

---
+++ AI/Layers/Layer.py
@@ -0,0 +1,67 @@
+import random
+import numpy as np
+from AI.NeuralNetwork.NeuralNetworkConstants import NeuralNetworkConstants
+from AI.NeuralNetwork.ActivationFunction import sigmoidFunc
+from AI.NeuralNetwork.ActivationFunction import dSigmoidFunc
+
+class layer:
+    weights = list()
+    deltaWeights = list()
+    def weightsInit(self, inputSize, outputSize):
+        weights = list()
+        for i in range((inputSize+1)*outputSize):
+            weights.append(random.uniform(-2, 2))
+        return weights
+
+    def __init__(self, inputSize, outputSize):
+        self.inputSize = inputSize+1
+        self.outputSize = outputSize
+        self.weights = self.weightsInit(inputSize, outputSize)
+        self.deltaWeights = np.zeros((inputSize+1)*outputSize)
+
+
+    def run(self, input):
+        self.input = input.copy()
+        self.output = []
+        self.input.append(1.0)
+        for i in range(self.outputSize):
+            tmp = 0.0
+            for j in range(self.inputSize):
+                tmp += self.input[j]*self.weights[j+i*self.inputSize]
+            self.output.append(sigmoidFunc(tmp))
+        return self.output.copy()
+
+
+    def train(self, error):
+        nextError = []
+        for i in range(self.inputSize):
+            nextError.append(0.0)
+        for i in range(self.outputSize):
+            delta = dSigmoidFunc(self.output[i])*error[i]
+            for j in range(self.inputSize):
+                nextError[j] = nextError[j] + delta*self.weights[j + i * self.inputSize]
+                deltaWeight = NeuralNetworkConstants.learningRate * self.input[j] * delta
+                self.weights[j + i * self.inputSize] += \
+                    deltaWeight + NeuralNetworkConstants.momentum * self.deltaWeights[j + i * self.inputSize]
+                self.deltaWeights[j + i * self.inputSize] = deltaWeight
+        return nextError
+
+    def save(self, n):
+        name = 'Layer'
+        name += n
+        name += '_weights.npy'
+        name1 = 'Layer'
+        name1 += n
+        name1 += '_deltaWeights.npy'
+        np.save(name, np.array(self.weights))
+        np.save(name1, np.array(self.deltaWeights))
+
+    def load(self,n):
+        name = 'Layer'
+        name += n
+        name += '_weights.npy'
+        name1 = 'Layer'
+        name1 += n
+        name1 += '_deltaWeights.npy'
+        self.weights = np.load(name)
+        self.deltaWeights = np.load(name1)
---
+++ App.py
@@ -0,0 +1,73 @@
+from AI.NeuralNetwork import neuralNetwork
+from AI.NeuralNetwork.NeuralNetworkConstants import NeuralNetworkConstants as Const
+from ImageConverter import ImageConverter
+from os import listdir
+from os.path import isfile, join
+import time
+
+
+class App:
+    @staticmethod
+    def create_tests():                             # creates tests input photos from row of numbers/sings
+        my_path = Const.pre_test_folder           # folder with photos to cut out
+        only_files = [f for f in listdir(my_path) if isfile(join(my_path, f))]
+
+        for path in only_files:
+            image_converter = ImageConverter(save_images=1)
+            input_data = image_converter.get_ai_input_data(my_path+"/"+path)
+
+    @staticmethod
+    def print_result(tmp):
+        max = 0
+        for j in range(14):
+            if tmp[j] > tmp[max]:
+                max = j
+        if max == 0:
+            second = 1
+        else:
+            second = 0
+        for j in range(14):
+            if tmp[j] > tmp[second] and j != max:
+                second = j
+        print(round(tmp[max] / sum(tmp) * 100, 2), end='% ')
+        print(Const.names[max], end=" ")
+        print(round(tmp[second] / sum(tmp) * 100, 2), end='% ')
+        print(Const.names[second], end=" ")
+
+    @staticmethod
+    def learn_machine():
+        start = time.time()
+        training_path = Const.training_folder
+        training_data_folders = [c for c in listdir(training_path)]  # get list of folders with data
+        folder_nr = 0
+        neural_network = neuralNetwork(600, 400, 14)
+        neural_network.load()
+        for folder in training_data_folders:
+            folder_path = training_path + "/" + folder
+            training_files = [f for f in listdir(folder_path) if isfile(join(folder_path, f))]
+            for file in training_files:
+                file_path = folder_path + "/" + file
+                training_data = ImageConverter.get_raw_data(path=file_path)
+                neural_network.train(training_data, Const.training_result[folder_nr])
+                print(file_path + " done.")
+
+            print(folder_path + " done. Saving all changes to neural network.")
+            neural_network.save()
+            folder_nr += 1
+        print("Time needed: +" + str(time.time() - start))
+
+    @staticmethod
+    def main(path):
+        path = "./zdj.jpg"
+        image_converter = ImageConverter(save_images=1)
+        input_data = image_converter.get_ai_input_data(path)
+
+        #  prepare objects for AI to work
+        x = neuralNetwork(600, 400, 14)
+        x.load()
+        start = time.time()
+        for data in input_data:
+            result = x.run(data)
+            App.print_result(result)
+            print("")
+        print("Time needed: +" + str(time.time() - start))
